122c0b6ea7dcc5e5af6ad0dbfacbfd5f2651865a
Samantha Bhor <audreybhorguzman@gmail.com>
2024-08-06 17:49:06 -0600

    textos agregados

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -4,25 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.testapp.ui.theme.TestappTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent { //  no tocar
+        setContent { // no tocar
             TestappTheme { // no tocar
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding -> //  no tocar
-                    Greeting( // trabajar sobre esto
-                        name = "Samantha",
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding -> // no tocar
+                    MyApp(
+                        name = "Dear User",
                         modifier = Modifier.padding(innerPadding)
+
                     )
                 }
             }
@@ -31,20 +36,63 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun MyApp(name: String, modifier: Modifier = Modifier) {
+    var showMessage by remember { mutableStateOf(false) }
+
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Greeting(name = name)
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Text(
+            text = "Bienvenido a CatGame ;)",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF1E88E5)
+
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "Esperamos que te diviertas mucho",
+            fontSize = 20.sp
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "Para continuar por favor presiona el botón de abajo",
+            fontSize = 16.sp
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+
+
+        // Greeting
+        Spacer(modifier = Modifier.height(32.dp))
+    }
+}
+
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
+        fontSize = 24.sp,
+        fontWeight =  FontWeight.Bold
     )
 }
 
 @Preview(
     showBackground = true,
-    showSystemUi = true //  habilita la interfaz para ver un preview
+    showSystemUi = true // habilita la interfaz para ver un preview
 )
 @Composable
-fun GreetingPreview() {
+fun MyAppPreview() {
     TestappTheme {
-        Greeting("Samantha")
+        MyApp(name = "Dear User")
     }
 }
